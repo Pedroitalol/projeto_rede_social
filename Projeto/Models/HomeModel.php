e5c77a0540e92c0350013b1413d348096fff9e14
Pedro Ítalo <pedroitalomatos@gmail.com>
2021-09-17 15:52:27 -0300

    Função para implementar imagens das postagens usando links feita

--- Projeto/Models/HomeModel.php
+++ Projeto/Models/HomeModel.php
@@ -6,6 +6,11 @@
             $pdo = \Projeto\MySql::connect();
             
             $post = strip_tags($post);
+            if(preg_match("/\[imagem=/", $post)){
+                $post = preg_replace("/(.*?)\[imagem=(.*?)\]/", '<p>$1</p><img src="$2"/>', $post);
+            }else{
+                $post = "<p>".$post."</p>";
+            }
 
             $postFeed = $pdo->prepare("INSERT INTO posts VALUES (null, ?, ?, ?)");
             $postFeed->execute(array($_SESSION["id"], $post, date("Y-m-d H:i:s", time())));
